4518cb1a07fbf0410afe216ab86564a0c5d2a1d7
Ryan Ramage <ryan.ramage@gmail.com>
2015-02-05 13:51:44 -0700

    add option to output the geocoded lat,lon of the hash

--- bin/cli.js
+++ bin/cli.js
@@ -1,27 +1,39 @@
 var hasher = require('..');
 var through2 = require('through2');
+var geohash = require('ngeohash');
 
 var options = require('rc')('geohash-poly', {
   precision: 7,
   hashMode: 'inside',
   rowMode: true,
-  csv: false
+  csv: false,
+  geocode: false
 })
 
 options.coords = JSON.parse(options._);
 
-var output = function(i){
-  console.log(JSON.stringify({hash: i}))
+var output = function(i, lat, lon){
+  var obj = {hash: i};
+  if (options.geocode){
+    obj.lat = lat; obj.lon = lon;
+  }
+  console.log(JSON.stringify(obj))
 }
 
-if (options.csv) output = function(i) { // see #1 for
-  console.log(i);
+if (options.csv) output = function(i, lat, lon) {
+  var arr = [i];
+  if (options.geocode){
+    arr.push(lat);
+    arr.push(lon);
+  }
+  console.log(arr.join(','));
 }
 
 hasher.stream(options).pipe(through2.obj(function (chunk, enc, cb) {
 
   chunk.toString().split(',').forEach(function(i){
-    output(i);
+    var coords = geohash.decode(i);
+    output(i, coords.latitude, coords.longitude);
   })
   cb();
 }));
